9f07695014db046c05e743e7f60d0054d4be1280
Maruen Mehana <maruen@gmail.com>
2016-04-05 22:47:21 -0300

    Refactorings

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,12 +3,17 @@ package controllers;
 import play.*;
 import play.mvc.*;
 
-import views.html.*;
+import views.html.index;
+import views.html.template;
 
 public class Application extends Controller {
 
     public Result index() {
         return ok(index.render());
+    }
+    
+    public Result template() {
+        return ok(template.render());
     }
 
 }
