edc7512fcba8715fe9e1ad6be314fbb02f8d4d79
Maruen Mehana <maruen@gmail.com>
2016-04-22 12:33:53 -0300

    HIGHLIGHT INCLUSIO

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -98,7 +98,7 @@
 		$('[data-countdown]').each(function() {
 		  var $this = $(this), finalDate = $(this).data('countdown');
 		  $this.countdown(finalDate, function(event) {
-			$this.html(event.strftime('<span class="cdown days"><span class="time-count">%-D</span> <p>Days</p></span> <span class="cdown hour"><span class="time-count">%-H</span> <p>Hour</p></span> <span class="cdown minutes"><span class="time-count">%M</span> <p>Min</p></span> <span class="cdown second"> <span><span class="time-count">%S</span> <p>Sec</p></span>'));
+			$this.html(event.strftime('<span class="cdown days"><span class="time-count">%-D</span> <p>Dias</p></span> <span class="cdown hour"><span class="time-count">%-H</span> <p>Horas</p></span> <span class="cdown minutes"><span class="time-count">%M</span> <p>Min</p></span> <span class="cdown second"> <span><span class="time-count">%S</span> <p>Seg</p></span>'));
 		  });
 		});	
 	// scroolup
